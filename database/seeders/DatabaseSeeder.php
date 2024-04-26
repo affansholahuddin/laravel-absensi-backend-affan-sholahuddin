9a6f7bef5605d18dacbcedc1d25a69b23941cda9
affansholahuddin33@gmail.com <affansholahuddin33@gmail.com>
2024-04-26 19:06:48 +0700

    feat:create company dashboard and api company

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,12 +14,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //data dummy for user
         User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Udin',
             'email' => 'udin@ash.com',
             'password' => Hash::make('12345678'),
+        ]);
+
+        //data dummy for company
+        \App\Models\Company::create([
+            'name' => 'PT Ash',
+            'email' => 'contact@ash.com',
+            'address' => 'Jalan Prof. HR. Boenyamin Pabuwaran, Purwokerto Utara, Banyumas, Jawa Tengah',
+            'latitude' => '-7.399757',
+            'longitude' => '109.244768',
+            'radius_km' => '0.5',
+            'time_in' => '08:00',
+            'time_out' => '17:00',
         ]);
     }
 }
